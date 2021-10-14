9deac967d02755589618dd7d60dd4906607bc2cc
drkiler <drkiler0901@gmail.com>
2021-10-14 17:56:38 +0800

    Delete videoa.mp4

---
+++ about/Untitled-1.js
@@ -0,0 +1,12 @@
+document.getElementById('menubtn').addEventListener('click', function (e) {
+  $('#menuUl').toggleClass('active');
+})
+
+$('#menuUl li').click(function () {
+  $(this).parent().find('li').each(function () {
+      if ($(this).hasClass('current-active')) {
+          $(this).toggleClass('current-active');
+      }
+  })
+  $(this).toggleClass('current-active');
+})
